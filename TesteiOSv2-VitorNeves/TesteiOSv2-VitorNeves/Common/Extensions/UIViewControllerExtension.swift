d6dfb58f38d309c522cb7c969c7bf822c08b1f3b
Vitor neves <j.victtor.neves@hotmail.com>
2019-03-04 18:54:38 -0300

    ViewController custom

---
+++ TesteiOSv2-VitorNeves/TesteiOSv2-VitorNeves/Common/Extensions/UIViewControllerExtension.swift
@@ -0,0 +1,28 @@
+//
+//  UIViewControllerExtension.swift
+//  TesteiOSv2-VitorNeves
+//
+//  Created by joao.vitor.f.naves on 04/03/2019.
+//  Copyright © 2019 joao.vitor.f.naves. All rights reserved.
+//
+
+import UIKit
+import SVProgressHUD
+
+extension UIViewController {
+
+    func showAlert(title: String, message: String){
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        let alertActionCancel = UIAlertAction(title: "Ok", style: UIAlertAction.Style.cancel, handler: nil)
+        alert.addAction(alertActionCancel)
+        self.present(alert,animated: true)
+    }
+    
+    func showHUD(){
+        SVProgressHUD.show()
+    }
+    
+    func dismissHUD() {
+        SVProgressHUD.dismiss()
+    }
+}
